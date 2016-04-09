53606df2a58b7e60f938e4d367ac34280755de38
Stepan Koci <stepan.koci@imatic.cz>
2016-04-09 07:17:59 +0200

    Progres.

--- php/1-php-html.php
+++ php/1-php-html.php
@@ -3,19 +3,21 @@
 // Jednoduchý program
 // Vypíše se jen výstup (echo), co není ve značkách php zůstává nezměněno
 
-$currentHour = date('H');
+$currentHour = date('G');
 
 // Ve dne vypíšeme den a v noci noc.
 if ($currentHour > 6 && $currentHour < 22) {
-    echo 'Den';
+    $now = 'den';
 } else {
-    echo 'Noc';
+    $now = 'noc';
 }
 
 ?>
 
 <p>
-    Mimo bloky PHP může být HTML kód.
-    PHP je zpracováno pouze uvnitř PHP značek.
+    Mimo bloky PHP může být HTML kód.<br>
+    PHP je zpracováno pouze uvnitř PHP značek.<br>
     Cokoliv je mimo značky, zůstane tak jak je.
 </p>
+
+<p><strong>Právě je <?php echo $now; ?> protože je <?php echo $currentHour ?> hodin.</strong></p>
